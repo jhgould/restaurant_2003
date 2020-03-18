7c8bd979e6c5be34c83eb9026459f9d8d95ac52a
Jack Gould <johnhilsmangould@gmail.com>
2020-03-18 14:00:15 -0600

    Add challenges

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,45 @@
+# restaurant class
 
+
+class Restaurant
+  attr_reader :opening_time, :name
+
+
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+
+  end
+
+  def dishes
+    @dishes
+  end
+
+  def closing_time(closing_time)
+    @opening_time = opening_time.to_i
+    closing = @opening_time + closing_time
+    closing = "#{closing}:00"
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+  def open_for_lunch?
+    if @opening_time.to_i < 12
+      true
+    else
+      false
+    end
+  end
+  require 'pry'; binding.pry
+
+  def cap_dishes
+    cap_dishes_array = []
+    @dishes.each do |dish|
+      cap_dishes_array << dish.upcase
+    end
+  end
+
+end
